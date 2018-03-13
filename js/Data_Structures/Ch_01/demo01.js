d6b7209f90a5040a16638875d053c5f377f395b3
xujiajun01 <xujiajun01@baidu.com>
2018-03-13 20:19:18 +0800

    写了篇总结

---
+++ js/Data_Structures/Ch_01/demo01.js
@@ -0,0 +1,9 @@
+let x=3;
+let y= 1.1;
+
+console.log(x+y);
+console.log(x*y);
+console.log(Math.sqrt(9));
+console.log(Math.abs(-1));
+
+
---
+++ js/Data_Structures/Ch_01/demo02.js
@@ -0,0 +1,2 @@
+let pi=3.141592653;
+console.log(pi.toFixed(2));
